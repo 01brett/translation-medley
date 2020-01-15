5f98674caae26b328a07097001198382508c4739
brett f <1brettf@gmail.com>
2020-01-15 13:45:29 -0800

    Implement ESV translation

--- src/actions/passageActions.js
+++ src/actions/passageActions.js
@@ -10,16 +10,16 @@ export const fetchPassage = passage => dispatch => {
     .get(`https://api.biblia.com/v1/bible/content/${passage.translation}.json?passage=${passage.location}&style=oneVersePerLine&key=fd37d8f28e95d3be8cb4fbc37e15e18e`)
     .then(res => {
       return res.data.text
-      .split(/\r\n/)
-      .splice(1, res.data.text.length - 1)
-      .map(el => {
-        const arr = el.split(/(?<=[0-9])(?=[\D])/);
-        return {
-          translation: passage.translation,
-          verseNumber: arr[0],
-          text: arr[1]
-        }
-      })
+        .split(/\r\n/)
+        .splice(1, res.data.text.length - 1)
+        .map(el => {
+          const arr = el.split(/(?<=[0-9])(?=[\D])/);
+          return {
+            translation: passage.translation,
+            verseNumber: arr[0],
+            text: arr[1]
+          }
+        })
     })
     .then(formattedPassage => (
       dispatch(
@@ -59,6 +59,47 @@ export const fetchNETPassage = location => dispatch => {
         }
       });
     })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: FETCH_PASSAGE_FAILURE, payload: err.response });
+    });
+};
+
+export const fetchESVPassage = location => dispatch => {
+  dispatch({ type: FETCH_PASSAGE_START });
+  axios
+    .get(`https://api.esv.org/v3/passage/text/?q=${location}&include-passage-references=false&include-footnotes=false&include-headings=false&include-copyright=false&include-short-copyright=false`, {
+      headers: {
+        'Authorization': 'Token 1410f911e8d537a0c57c1219c101ecf84540d6c4'
+      }
+    })
+    .then(res => {
+      return res.data.passages[0]
+        .replace(/([[\]])/g, '')
+        .replace(/\n\n/g, '')
+        .replace(/\s{2}/g, ' ')
+        .split(/[ ]+(?=\d)/g)
+        .splice(1, res.data.passages[0].length - 1)
+        .map(el => {
+          const arr = el.split(/(?<=[0-9])(?=[\D])/);
+          return {
+            translation: 'ESV',
+            verseNumber: arr[0],
+            text: arr[1]
+          }
+        })
+    })
+    .then(formattedPassage => (
+      dispatch(
+        {
+          type: FETCH_PASSAGE_SUCCESS,
+          payload: {
+            translation: 'ESV',
+            content: formattedPassage
+          }
+        }
+      )
+    ))
     .catch(err => {
       console.log(err);
       dispatch({ type: FETCH_PASSAGE_FAILURE, payload: err.response });
