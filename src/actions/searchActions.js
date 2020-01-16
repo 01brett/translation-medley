cc9a7d2dc6845a7ad372bf948fe3075c35da76ff
brett f <1brettf@gmail.com>
2020-01-16 09:56:28 -0800

    Passage swap disabled on same translation

---
+++ src/actions/searchActions.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+import { fetchPassage, fetchESVPassage, fetchNETPassage } from './passageActions'
+
+export const searchPassage = (trans, location) => dispatch => {
+
+}
